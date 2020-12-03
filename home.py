afafb0c6cbd4eff7f4ed270facc150f6d9c8bfb5
UNDA <55609205+undauniversity@users.noreply.github.com>
2020-12-03 20:10:08 +0700

    Add files via upload

--- home.py
+++ home.py
@@ -1,24 +1,25 @@
 from kivy.app import App
 from kivymd.app import MDApp
 
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.boxlayout import MDBoxLayout
-
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 
 import urllib.request, json
 
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout
 
 def cekLogin(username, password):
     with urllib.request.urlopen("http://localhost/lat/api.php?auth=888&perintah=loginMhs&user="+username+"&pass="+password) as json_url:
         data = json.loads(json_url.read())
-        #print(data)
-        #print(json.dumps(data, indent=3, sort_keys=True))
-        #print("isi data=" +username +", " +password)
         usernameTabel = data[0]["npm"]
         passwordTabel = data[0]["password"]
 
+
+        #root.manager.current = 'Beranda'
         if username==usernameTabel and password==passwordTabel:
             print("Login Berhasil")
             data=1
@@ -28,20 +29,36 @@ def cekLogin(username, password):
 
     return data
 
+class LoginScreen(Screen):
+    pass
 
+
+class HomeScreen(Screen):
+    pass
+
+
+class SignupScreen(Screen):
+    pass
+
+'''
 class MylayoutForm(MDBoxLayout):
     def doLogin(self):
         #print("Explicit is better than implicit.")
         #self.txtPassword_.text="halo"
         cekLogin(self.txtUsername_.text,self.txtPassword_.text) #cek isi dari txtUsername_ dan txtPassword_
+'''
+
+class LoginScreen(MDBoxLayout):
+   def doLogin(self):
+        print("Halooo")
 
 # penamaan class harus sama dengan file kv
 # mylayout.kv
 # nama class WAJIB MylayoutApp
-class MylayoutApp(MDApp):
+class SimApp(MDApp):
     pass
 if __name__ == '__main__':
      def build(self):
         self.theme_cls.primary_palette = "Blue"
-MylayoutApp().run()
+SimApp().run()
         
